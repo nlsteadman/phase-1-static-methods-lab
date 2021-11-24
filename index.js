2206e58d61a9b7790f3b57021b725cfe7b58c64f
Nicole Steadman <nsteadman@gmail.com>
2021-11-24 11:00:04 -0800

    commit tree 0fce5982389c64cf6fd3c4e41cfda2ba8ab83879

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(sentence) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let array = sentence.split(' ');
+    let result = [];
+
+    for(let n = 0; n < array.length; n++) {
+      if (n == 0) {
+        result.push(this.capitalize(array[n]))
+      }
+      else {
+        if (exceptions.includes(array[n])) {
+          result.push(array[n])
+        }
+        else {
+          result.push(this.capitalize(array[n]))
+        }
+      }
+    }
+    return result.join(' ');
+  }
 }
+
